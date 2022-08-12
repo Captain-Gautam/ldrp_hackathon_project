a45d38968b0d038e179e547453b7c65ab5f34774
ripslinger17 <smit.p1707@gmail.com>
2022-08-13 00:21:37 +0530

    second commit

--- ldrp_hackathon/ldrp_hackathon/settings.py
+++ ldrp_hackathon/ldrp_hackathon/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ldrp_hackathon',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
