8e45e0725e823acb3b773ab06ef8b21f9c2a6c67
ripslinger17 <smit.p1707@gmail.com>
2022-08-14 09:29:11 +0530

    15th commit

--- project1/index/models.py
+++ project1/index/models.py
@@ -1,15 +1,35 @@
 from django.db import models
 
 # Create your models here.
-COLOR_CHOICES = (
-    ('green','GREEN'),
-    ('blue', 'BLUE'),
-    ('red','RED'),
-    ('orange','ORANGE'),
-    ('black','BLACK'),
+symptomsF= (
+    ('Heart Disease','Heart Diseases'),
+    ('Breast Cancer','Breast Cancer'),
+    ('Ovarian and Cervical Cancer','Ovarian and Cervical Cancer'),
+    ('Gynecological Health','Gynecological Health'),
+    ('Pregnancy Issues','Pregnancy Issues'),
+    ('Depression and Anxiety','Depression and Anxiety'),
+    ('Pneumonia','Pneumonia'),
+    ('Diarrhoe','Diarrhoe'),
+    ('Malaria','Malaria'),
+    ('Tuberculosis','Tuberculosis'),
+    ('Chickenpox','Chickenpox'),
+    ('Whooping cough','Whooping cough'),
+    ('Common Cold','Common Cold'),
+    ('Ear infection','Ear infection'),
+    ('Gastroenteritis','Gastroenteritis'),
+    ('Influenza (flu)','Influenza (flu)'),
+    ('Strep throat','Strep throat'),
+    ('Mononucleosis','Mononucleosis'),
+    ('Gastroenteritis','Gastroenteritis'),
+    ('Anxiety','Anxiety'),
+    ('Asthma','Asthma'),
+    ('Cancer','Cancer'),
+    ('Allergy','Allergy'),
+    ('Gonorrhea','Gonorrhea'),
+    ('Syphilis','Syphilis'),
+    ('HIV','HIV'),
+    ('Chlamydia','Chlamydia'),
 )
 
-
 class Symptom(models.Model):
-     symptom1 = models.CharField(max_length=50, choices=COLOR_CHOICES,)
-   
+     symptom1 = models.CharField(max_length=50, choices=symptomsF)
